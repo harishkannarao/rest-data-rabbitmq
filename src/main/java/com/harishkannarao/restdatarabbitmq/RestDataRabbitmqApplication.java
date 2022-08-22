ef91dc71a214f6c769be0238a2d22d1cb2bcc5b7
harishkannarao <harishkannarao@users.noreply.github.com>
2022-08-22 23:19:04 +0100

    Initial Commit

---
+++ src/main/java/com/harishkannarao/restdatarabbitmq/RestDataRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.restdatarabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDataRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDataRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harishkannarao/restdatarabbitmq/RestDataRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harishkannarao.restdatarabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDataRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
