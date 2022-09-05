cf670fce4495b04d280317ca198ec401d583cb76
harishkannarao <harishkannarao@users.noreply.github.com>
2022-09-05 16:43:16 +0100

    Eliminating @Jacksonized

--- src/main/java/com/harishkannarao/restdatarabbitmq/entity/SampleMessage.java
+++ src/main/java/com/harishkannarao/restdatarabbitmq/entity/SampleMessage.java
@@ -2,13 +2,11 @@ package com.harishkannarao.restdatarabbitmq.entity;
 
 import lombok.Builder;
 import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 import java.util.UUID;
 
 @Value
 @Builder(toBuilder = true)
-@Jacksonized
 public class SampleMessage {
     UUID id;
     String value;
