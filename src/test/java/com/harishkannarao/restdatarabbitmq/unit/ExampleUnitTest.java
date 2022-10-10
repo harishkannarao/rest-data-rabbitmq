a9c397430d198c9c10964ea1ddb23f4e83da86d7
harishkannarao <harishkannarao@users.noreply.github.com>
2022-10-10 23:21:15 +0100

    Sample unit tests

---
+++ src/test/java/com/harishkannarao/restdatarabbitmq/unit/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.harishkannarao.restdatarabbitmq.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExampleUnitTest {
+    @Test
+    public void test_example_1() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    @Test
+    public void test_example_2() {
+        assertThat(true).isEqualTo(true);
+    }
+}
---
+++ src/test/java/com/harishkannarao/restdatarabbitmq/unit/SampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.harishkannarao.restdatarabbitmq.unit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SampleUnitTest {
+    @Test
+    public void test_sample_1() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    @Test
+    public void test_sample_2() {
+        assertThat(true).isEqualTo(true);
+    }
+}
