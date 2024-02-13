626991bc104aa1846106aa1eb22fe8dadaecadfa
harishkannarao <harishkannarao@users.noreply.github.com>
2024-02-13 16:28:02 +0000

    Creating scenario for retry and success

--- src/test/java/com/harishkannarao/restdatarabbitmq/steps/MessageListenerSteps.java
+++ src/test/java/com/harishkannarao/restdatarabbitmq/steps/MessageListenerSteps.java
@@ -37,13 +37,25 @@ public class MessageListenerSteps extends AbstractBaseSteps {
         messageListenerHolder.getSampleMessages().put(canonicalName, sampleMessage);
     }
 
-    @When("I send sample message {string} to message-processor.inbound-topic-exchange with message-processor.inbound-routing-key")
-    public void iSendSampleMessageToMessageProcessorInboundTopicExchangeWithMessageProcessorInboundRoutingKey(String canonicalName) {
+    @Given("a random sample message called {string} with value {string}")
+    public void aRandomSampleMessageWithValue(String canonicalName, String value) {
+        SampleMessage sampleMessage = SampleMessage.builder()
+                .id(UUID.randomUUID())
+                .value(value)
+                .build();
+        messageListenerHolder.getSampleMessages().put(canonicalName, sampleMessage);
+    }
+
+    @When("I send sample message {string} to {string} with {string}")
+    public void iSendSampleMessageToMessageProcessorInboundTopicExchangeWithMessageProcessorInboundRoutingKey(
+            String canonicalName,
+            String inboundTopicExchange,
+            String inboundRoutingKey) {
         SampleMessage sampleMessage = messageListenerHolder.getSampleMessages().get(canonicalName);
         List<SampleMessage> inputMessages = List.of(sampleMessage);
         String message = jsonConverter().toJson(inputMessages);
-        String topicExchange = getProperty("messaging.message-processor.inbound-topic-exchange");
-        String routingKey = getProperty("messaging.message-processor.inbound-routing-key");
+        String topicExchange = getProperty(inboundTopicExchange);
+        String routingKey = getProperty(inboundRoutingKey);
         Map<String, Object> headers = Map.of("X-Correlation-ID", sampleMessage.getId());
         rabbitMessagingTemplate().convertAndSend(topicExchange, routingKey, message, headers);
     }
@@ -51,7 +63,7 @@ public class MessageListenerSteps extends AbstractBaseSteps {
     @Then("I should see sample message {string} in TestMessageListener")
     public void iShouldSeeSampleMessageInTestMessageListener(String canonicalName) {
         SampleMessage sampleMessage = messageListenerHolder.getSampleMessages().get(canonicalName);
-        await().atMost(Duration.ofSeconds(5)).until(() -> TestMessageListener.HOLDER.containsKey(sampleMessage.getId()));
+        await().atMost(Duration.ofSeconds(20)).until(() -> TestMessageListener.HOLDER.containsKey(sampleMessage.getId()));
         var result = TestMessageListener.HOLDER.get(sampleMessage.getId());
         assertThat(result.getValue()).isEqualTo(sampleMessage.getValue());
     }
--- src/test/resources/com/harishkannarao/features/listener/message-listener.feature
+++ src/test/resources/com/harishkannarao/features/listener/message-listener.feature
@@ -1,7 +1,13 @@
 Feature: message-listener
 
-  Scenario: message-listener receives and sends message
+  Scenario: message-listener receives inbound message and sends outbound message
     Given I clear messages in TestMessageListener
     Given a random sample message called "sample_message"
-    When I send sample message "sample_message" to message-processor.inbound-topic-exchange with message-processor.inbound-routing-key
+    When I send sample message "sample_message" to "messaging.message-processor.inbound-topic-exchange" with "messaging.message-processor.inbound-routing-key"
+    Then I should see sample message "sample_message" in TestMessageListener
+
+  Scenario: message-listener receives inbound message then retries and sends outbound message
+    Given I clear messages in TestMessageListener
+    Given a random sample message called "sample_message" with value "$$"
+    When I send sample message "sample_message" to "messaging.message-processor.inbound-topic-exchange" with "messaging.message-processor.inbound-routing-key"
     Then I should see sample message "sample_message" in TestMessageListener
