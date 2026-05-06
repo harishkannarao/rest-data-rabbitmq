40f598a01806275e8ddce20bc05ae4551f7377bd
harishkannarao <harishkannarao@users.noreply.github.com>
2026-05-06 22:16:39 +0100

    First Publisher Integration Test

---
+++ src/test/java/com/harishkannarao/restdatarabbitmq/integration/MessagePublisherIT.java
@@ -0,0 +1,51 @@
+package com.harishkannarao.restdatarabbitmq.integration;
+
+import com.harishkannarao.restdatarabbitmq.entity.SampleMessage;
+import com.harishkannarao.restdatarabbitmq.listener.TestMessageListener;
+import com.harishkannarao.restdatarabbitmq.publisher.MessagePublisher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+public class MessagePublisherIT extends AbstractBaseIntegration {
+
+    private MessagePublisher messagePublisher;
+
+    @BeforeEach
+    public void setUp() {
+        messagePublisher = getBean(MessagePublisher.class);
+    }
+
+    @Override
+    protected Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("test.store-received-messages", "true");
+        return properties;
+    }
+
+    @Test
+    public void successfully_publish_to_outbound_queue() {
+        SampleMessage sampleMessage = SampleMessage.builder()
+                .id(UUID.randomUUID())
+                .value("Hello World" + UUID.randomUUID())
+                .build();
+
+        messagePublisher.sendToOutboundQueue(sampleMessage);
+
+        await()
+                .atMost(Duration.ofSeconds(3))
+                .untilAsserted(() -> {
+                    Map<UUID, SampleMessage> publishedMessages = Map.copyOf(TestMessageListener.HOLDER);
+                    assertThat(publishedMessages)
+                            .hasEntrySatisfying(sampleMessage.getId(),
+                                    actualMessage -> assertThat(actualMessage).isEqualTo(sampleMessage));
+                });
+    }
+}
