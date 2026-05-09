cc0d84c5501588c21ed11aefbe73d21dc33f0274
harishkannarao <harishkannarao@users.noreply.github.com>
2026-05-09 20:58:27 +0100

    Declaring dead letter queue

--- src/test/java/com/harishkannarao/restdatarabbitmq/config/RabbitMqConfiguration.java
+++ src/test/java/com/harishkannarao/restdatarabbitmq/config/RabbitMqConfiguration.java
@@ -1,9 +1,6 @@
 package com.harishkannarao.restdatarabbitmq.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -15,7 +12,10 @@ public class RabbitMqConfiguration {
 
     @Bean(name = "messageProcessorInboundQueue")
     Queue messageProcessorInboundQueue(@Value("${messaging.message-processor.inbound-queue}") String messageProcessorInboundQueue) {
-        return new Queue(messageProcessorInboundQueue, false);
+        return QueueBuilder.nonDurable(messageProcessorInboundQueue)
+                .withArgument("x-dead-letter-exchange", messageProcessorInboundQueue + ".dlx")
+                .withArgument("x-dead-letter-routing-key", messageProcessorInboundQueue + ".dlq.rk")
+                .build();
     }
 
     @Bean(name = "messageProcessorInboundTopicExchange")
@@ -25,9 +25,26 @@ public class RabbitMqConfiguration {
 
     @Bean
     Binding inboundBinding(@Qualifier("messageProcessorInboundQueue") Queue queue,
-                    @Qualifier("messageProcessorInboundTopicExchange") TopicExchange exchange,
-                    @Value("${messaging.message-processor.inbound-routing-key}") String messageProcessorInboundRoutingKey) {
+                           @Qualifier("messageProcessorInboundTopicExchange") TopicExchange exchange,
+                           @Value("${messaging.message-processor.inbound-routing-key}") String messageProcessorInboundRoutingKey) {
         return BindingBuilder.bind(queue).to(exchange).with(messageProcessorInboundRoutingKey);
+    }
+
+    @Bean(name = "messageProcessorInboundDlq")
+    Queue messageProcessorInboundDlq(@Value("${messaging.message-processor.inbound-queue}") String messageProcessorInboundQueue) {
+        return QueueBuilder.nonDurable(messageProcessorInboundQueue + ".dlq").build();
+    }
+
+    @Bean(name = "messageProcessorInboundDlqExchange")
+    TopicExchange deadLetterExchange(@Value("${messaging.message-processor.inbound-queue}") String queueName) {
+        return new TopicExchange(queueName + ".dlx");
+    }
+
+    @Bean
+    Binding inboundDlqBinding(@Qualifier("messageProcessorInboundDlq") Queue queue,
+                              @Qualifier("messageProcessorInboundDlqExchange") TopicExchange exchange,
+                              @Value("${messaging.message-processor.inbound-queue}") String queueName) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName + ".dlq.rk");
     }
 
     @Bean(name = "messageProcessorInboundRetryQueue")
@@ -44,8 +61,8 @@ public class RabbitMqConfiguration {
 
     @Bean
     Binding inboundRetryBinding(@Qualifier("messageProcessorInboundRetryQueue") Queue queue,
-                           @Qualifier("messageProcessorInboundRetryTopicExchange") TopicExchange exchange,
-                           @Value("${messaging.message-processor.inbound-retry-routing-key}") String messageProcessorInboundRetryRoutingKey) {
+                                @Qualifier("messageProcessorInboundRetryTopicExchange") TopicExchange exchange,
+                                @Value("${messaging.message-processor.inbound-retry-routing-key}") String messageProcessorInboundRetryRoutingKey) {
         return BindingBuilder.bind(queue).to(exchange).with(messageProcessorInboundRetryRoutingKey);
     }
 
@@ -61,8 +78,8 @@ public class RabbitMqConfiguration {
 
     @Bean
     Binding outboundBinding(@Qualifier("messageProcessorOutboundQueue") Queue queue,
-                           @Qualifier("messageProcessorOutboundTopicExchange") TopicExchange exchange,
-                           @Value("${messaging.message-processor.outbound-routing-key}") String messageProcessorOutboundRoutingKey) {
+                            @Qualifier("messageProcessorOutboundTopicExchange") TopicExchange exchange,
+                            @Value("${messaging.message-processor.outbound-routing-key}") String messageProcessorOutboundRoutingKey) {
         return BindingBuilder.bind(queue).to(exchange).with(messageProcessorOutboundRoutingKey);
     }
 
